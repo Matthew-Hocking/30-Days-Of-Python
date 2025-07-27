cb8bc1a9c39d0a32de97ab4c9734ca8d31f686c7
Matthew-Hocking <matthocking2439@gmail.com>
2025-07-27 12:39:41 +1200

    Learn python data types, math operators, and other basics

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -14,10 +14,18 @@ print(3 // 2)  # Floor division operator(//)
 print(type(10))                  # Int
 print(type(3.14))                # Float
 print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
-print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Set
-print(type((9.8, 3.14, 2.7)))    # Tuple
+print(type('Matthew'))          # String
+print(type([1, 2, 3]))           # List, an unordered collection to store different data types
+print(type({'name':'Matthew'})) # Dictionary, an unordered collection of data in a key value pair format
+print(type({9.8, 3.14, 2.7}))    # Set, a collection of data types similar to list and tuple, however, a set is not an ordered collection of items. A set only stores unique items.
+print(type((9.8, 3.14, 2.7)))    # Tuple, an ordered collection of different data types like list but tuples can not be modified once they are created. They are immutable.
 print(type(3 == 3))              # Bool
 print(type(3 >= 3))              # Bool
+
+# Solve the Euclidean distance between two points
+p = [2, 3]
+q = [10, 8]
+
+solution = ((q[0] - p[0]) ** 2 + (q[1] - p[1]) ** 2) ** 0.5
+
+print(f"The Euclidean distance between points p:{p} and q:{q} is: {round(solution, 2)} units")
